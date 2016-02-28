0d484704f6b3cf142676e46eb2432ada83838fd7
Austin <abarrett@biohitech.com>
2016-02-28 18:02:07 -0500

    Added resources

---
+++ assets/js/haha.js
@@ -0,0 +1,8 @@
+document.getElementById("bannerLogo").addEventListener("dblclick", function(){
+    var KICKASSVERSION = '2.0';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    document.body.appendChild(s);
+    s.src = '//hi.kickassapp.com/kickass.js';
+    void(0);
+});
